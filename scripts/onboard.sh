e2b8daced488541022eef548ed644dd17a14dd0c
Badr <choubai.badr@protonmail.com>
2024-03-22 21:27:17 -0600

    Use versioned route

--- scripts/onboard.sh
+++ scripts/onboard.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+curl http://localhost:9180/apisix/admin/plugin_configs/proxy_rewrite -H 'X-API-KEY: abcdefghijklmnopqrstuvwxyz' -X DELETE
 curl http://localhost:9180/apisix/admin/routes/1 -H 'X-API-KEY: abcdefghijklmnopqrstuvwxyz' -X DELETE
 curl http://localhost:9180/apisix/admin/upstreams/1 -H 'X-API-KEY: abcdefghijklmnopqrstuvwxyz' -X DELETE
+
+(sh ./onboard_plugins.sh)
 
 # https://apisix.apache.org/docs/apisix/tutorials/expose-api/#expose-your-service
 (curl http://localhost:9180/apisix/admin/upstreams/1 -s -H 'X-API-KEY: abcdefghijklmnopqrstuvwxyz' -X PUT -d '
@@ -27,6 +30,7 @@ curl http://localhost:9180/apisix/admin/upstreams/1 -H 'X-API-KEY: abcdefghijklm
 }')
 
 # Create Route w/ basic Upstream
+# shellcheck disable=SC2016
 (curl http://localhost:9180/apisix/admin/routes/1 -s -H 'X-API-KEY: abcdefghijklmnopqrstuvwxyz' -X PUT -d '
 {
   "desc": "User v1 route",
@@ -34,16 +38,18 @@ curl http://localhost:9180/apisix/admin/upstreams/1 -H 'X-API-KEY: abcdefghijklm
   "labels": { "environment": "development", "version": "v1" },
   "methods": ["GET"],
   "name": "Users V1",
+  "plugin_config_id": "proxy_rewrite",
   "status": 1,
   "upstream_id": 1,
-  "uris": ["/health", "/health/"]
+  "uri": "/api/v1/users/health*"
 }')
 
 echo
 echo
 echo
 
-echo "Service HTTP call:"
+#echo "Service HTTP call:"
 #(curl -i -X GET "http://localhost:8081/health")
 echo "Gateway HTTP call:"
-(curl -i -X GET "http://localhost:9080/health" -H 'Host: users-service-v1')
+#(curl -i -X GET "http://localhost:9080/health" -H 'Host: users-service-v1')
+(curl -i -X GET "http://localhost:9080/api/v1/users/health" -H 'Host: users-service-v1')
